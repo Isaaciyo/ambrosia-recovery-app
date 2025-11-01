a46bd00878890d24847584c83480b4af44e9fb31
Stiinj <129190683+Stiinj@users.noreply.github.com>
2025-11-01 18:41:24 -0500

    Update all files cuz prev version rendered blank screen

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/ambrosia-recovery-app/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
@@ -51,7 +52,7 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
